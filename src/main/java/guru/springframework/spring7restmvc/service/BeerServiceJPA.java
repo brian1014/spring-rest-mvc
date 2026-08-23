f844fbf66ac25392c66f5fb914087fd3e7ae3515
Brian Alvarez <brianalvarez2000@gmail.com>
2026-08-22 19:22:39 -0500

    Section 18 - Database Transaction OK, NO changes in code only one

--- src/main/java/guru/springframework/spring7restmvc/service/BeerServiceJPA.java
+++ src/main/java/guru/springframework/spring7restmvc/service/BeerServiceJPA.java
@@ -93,6 +93,8 @@ public class BeerServiceJPA implements BeerService {
             foundBeer.setBeerStyle(beerDTO.getBeerStyle());
             foundBeer.setUpc(beerDTO.getUpc());
             foundBeer.setPrice(beerDTO.getPrice());
+            foundBeer.setQuantityOnHand(beerDTO.getQuantityOnHand());
+            foundBeer.setVersion(beerDTO.getVersion());
             atomicReference.set(Optional.of(beerMapper.beerToBeerDTO(beerRepository.save(foundBeer))));
         }, () -> atomicReference.set(Optional.empty()));
 
