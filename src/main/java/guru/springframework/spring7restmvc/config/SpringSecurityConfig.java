70a192c3688d2d0838cd9d93d35b0300b0863843
Brian Alvarez <brianalvarez2000@gmail.com>
2026-08-31 16:45:03 -0500

    Section 40: OpenAPI with SpringBoot

--- src/main/java/guru/springframework/spring7restmvc/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring7restmvc/config/SpringSecurityConfig.java
@@ -13,7 +13,11 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
         httpSecurity
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(
+                                        "/v3/api-docs**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                                .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 ->
                     oauth2.jwt(Customizer.withDefaults())
                 );
